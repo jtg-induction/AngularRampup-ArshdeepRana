d2cbfb5034e8e557defe8e44f9003250a043cf23
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-09-11 11:52:45 +0530

    AR_RU_01 : Base setup : Used modular structure

---
+++ VibeReads/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './core/app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { appRoutes } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
