5af96528c14e42f1e9c644dd4adf4b7545329849
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-09-12 16:19:38 +0530

    AR_RU_02 : Signup : Added path aliases

--- VibeReads/src/app/app.module.ts
+++ VibeReads/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './core/components/app.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
+import { AppComponent } from '@core/components/app.component';
+import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
 import { appRoutes } from './app.routes';
 
 @NgModule({
